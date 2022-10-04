f8b9b1a2e08469fe5c7f159e302532a82ccaa56e
andre <andre@10.60.58.150>
2022-10-04 18:31:55 +0200

    Comienzo del proyecto

---
+++ Telemedicine_Project/src/main/java/Client/Patient.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package Client;
+
+/**
+ *
+ * @author andre
+ */
+
+import java.util.*;
+import java.time.*;
+
+public class Patient {
+    private String name;
+    private String lastname;
+    private Integer id;
+    private String gender;
+    private String address;
+    private String telephone;
+    private Date dateOfBirth;
+}
