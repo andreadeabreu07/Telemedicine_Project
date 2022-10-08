bbf53da80fca526147d6961d5cdaf2b33bbc280d
andre <andre@10.60.58.150>
2022-10-08 15:19:19 +0200

    Comienzo del proyecto

--- Telemedicine_Project/src/main/java/Client/Patient.java
+++ Telemedicine_Project/src/main/java/Client/Patient.java
@@ -11,14 +11,87 @@ package Client;
  */
 
 import java.util.*;
-import java.time.*;
+
 
 public class Patient {
+    
     private String name;
     private String lastname;
     private Integer id;
     private String gender;
     private String address;
     private String telephone;
-    private Date dateOfBirth;
+    private Date dob;
+    Signals signal;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public Signals getSignal() {
+        return signal;
+    }
+
+    public void setSignal(Signals signal) {
+        this.signal = signal;
+    }
+
+    @Override
+    public String toString() {
+        return "Patient{" + "name=" + name + ", lastname=" + lastname + ", id=" + id + ", gender=" + gender + ", address=" + address + ", telephone=" + telephone + ", dob=" + dob + ", signal=" + signal + '}';
+    }
+    
+    
 }
